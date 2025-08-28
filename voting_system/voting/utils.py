08eaf94df9ca5b642672f1b3ba75f0964c9c06ba
nandan017 <nandanashok456@gmail.com>
2025-08-28 13:14:26 +0530

    major update

--- voting_system/voting/utils.py
+++ voting_system/voting/utils.py
@@ -33,8 +33,18 @@ def decode_qr_code_from_frame(base64_image):
 def is_valid_student_qr(qr_code_data):
     """Validate the student QR code format."""
     current_year = datetime.datetime.now().year % 100  # Get the last two digits of the current year
-    if len(qr_code_data) == 7 and qr_code_data[:2].isdigit() and qr_code_data[2:4].isalnum() and qr_code_data[4:].isdigit():
+    if len(qr_code_data) == 7 and qr_code_data[:2].isdigit() and qr_code_data[2:4].isalnum() and qr_code_data[2:4].isupper() and qr_code_data[4:].isdigit():
         year = int(qr_code_data[:2])  # Extract the two-digit year
         if current_year - year in range(0, 4):  # Check if the year is within the valid range
             return True
-    return False
+    elif len(qr_code_data) == 8 and qr_code_data[:2].isdigit() and qr_code_data[2:5].isalnum() and qr_code_data[2:5].isupper() and qr_code_data[5:].isdigit():
+        year = int(qr_code_data[:2])
+        if current_year - year in range(0, 4):  # Check if the year is within the valid range
+            return True
+        return True
+    elif len(qr_code_data) == 7 and qr_code_data[:2].isdigit() and qr_code_data[2:5].isalnum() and qr_code_data[2:5].isupper() and qr_code_data[5:].isdigit():
+        year = int(qr_code_data[:2])  # Extract the two-digit year
+        if current_year - year in range(0, 4):  # Check if the year is within the valid range
+            return True
+    else:
+        return False
